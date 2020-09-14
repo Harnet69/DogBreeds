3baf2315cff1a7b9d2b04d2e28ee9e4663464d3b
Harnet69 <harnet69@gmail.com>
2020-09-14 12:11:42 +0200

    Added fragments

--- app/src/main/java/com/harnet/dogbreeds/view/MainActivity.kt
+++ app/src/main/java/com/harnet/dogbreeds/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.harnet.dogbreeds
+package com.harnet.dogbreeds.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.harnet.dogbreeds.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
