fb8357e8bea300fd7d0905105bed5ba9bd492124
Harnet69 <harnet69@gmail.com>
2020-09-15 10:33:20 +0200

    Implemented back arrow

--- app/src/main/java/com/harnet/dogbreeds/view/MainActivity.kt
+++ app/src/main/java/com/harnet/dogbreeds/view/MainActivity.kt
@@ -2,11 +2,26 @@ package com.harnet.dogbreeds.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.harnet.dogbreeds.R
 
 class MainActivity : AppCompatActivity() {
+    // back arrow
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //for back arrow
+        navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    //for back arrow
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
