986afb8bb5ec10371584e101c4c0c1c7099dec12
Harnet69 <harnet69@gmail.com>
2020-09-16 12:27:06 +0200

    Implemented dogBreed class

---
+++ app/src/main/java/com/harnet/dogbreeds/model/Model.kt
@@ -0,0 +1,11 @@
+package com.harnet.dogbreeds.model
+
+data class dogBreed(
+    val breedId: String?,
+    val dogBreed: String?,
+    val lifespan: String?,
+    val breedGrupp: String?,
+    val bredFor: String?,
+    val temperament: String?,
+    val inmageURL: String?
+)
