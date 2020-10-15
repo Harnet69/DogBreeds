91beb95f8cf2dc1ea149c34ead9a1e049cf24f5a
Harnet69 <harnet69@gmail.com>
2020-10-15 09:58:53 +0200

    Implemented transition to detail page from list

--- app/src/main/java/com/harnet/dogbreeds/util/Util.kt
+++ app/src/main/java/com/harnet/dogbreeds/util/Util.kt
@@ -3,14 +3,17 @@ package com.harnet.dogbreeds.util
 import android.app.Activity
 import android.content.Context
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.databinding.BindingAdapter
+import androidx.navigation.Navigation
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.harnet.dogbreeds.R
 import com.harnet.dogbreeds.view.DogsListAdapter
+import com.harnet.dogbreeds.view.ListFragmentDirections
 import java.util.concurrent.CompletableFuture
 
 //little loading spinner for image loading
@@ -35,7 +38,7 @@ fun ImageView.loadImage(uri: String?, progressDrawable: CircularProgressDrawable
 
 // load images with Glide library
 @BindingAdapter("android:bindImageUrl")
-fun loadBindingImage(view: ImageView, url: String?){
+fun loadBindingImage(view: ImageView, url: String?) {
     view.loadImage(url, getProgressDrawable(view.context))
 }
 
@@ -53,5 +56,18 @@ fun loadImageByOwnImageLoader(view: ImageView, url: String?) {
                     view.setImageBitmap(image)
                 })
             }
+    }
+}
+
+// go to appropriate dog detail page
+@BindingAdapter("android:goToDetailPage")
+fun goToDetailPage(view: View, breedId: String) {
+    view.setOnClickListener {
+        // navigate to appropriate detail fragment
+        val action = ListFragmentDirections.actionDetailFragment()
+        // send dog id to DetailFragment
+        action.dogId = breedId
+
+        Navigation.findNavController(it).navigate(action)
     }
 }
