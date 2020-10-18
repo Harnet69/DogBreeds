450ee8c4a8671dd6f0d0cc496d496d9b0fd22bd6
Harnet69 <harnet69@gmail.com>
2020-10-18 11:31:59 +0200

    Palette library was implemented

--- app/src/main/java/com/harnet/dogbreeds/view/DetailFragment.kt
+++ app/src/main/java/com/harnet/dogbreeds/view/DetailFragment.kt
@@ -1,5 +1,7 @@
 package com.harnet.dogbreeds.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,8 +11,13 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.harnet.dogbreeds.R
 import com.harnet.dogbreeds.databinding.FragmentDetailBinding
+import com.harnet.dogbreeds.model.DogPalette
 import com.harnet.dogbreeds.util.getProgressDrawable
 import com.harnet.dogbreeds.util.loadImage
 import com.harnet.dogbreeds.viewModel.DetailViewModel
@@ -49,7 +56,35 @@ class DetailFragment : Fragment() {
             // if dog isn't null
             dog?.let {
                 dataBinding.dogObj = dog
+
+                // set image to Palette
+                it.imageURL?.let { url ->
+                    setupBackgroundColor(url)
+                }
             }
         })
+    }
+
+    // Palette handler
+    private fun setupBackgroundColor(url: String){
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object : CustomTarget<Bitmap>(){
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate {palette->
+                            //extract color. If rgb is null intColor = 0
+                            val intColor = palette?.vibrantSwatch?.rgb ?: 0
+                            //create an object of Palette
+                            val articlePalette = DogPalette(intColor)
+                            //bind object to View xml
+                            dataBinding.palette = articlePalette
+                        }
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {
+                }
+            })
     }
 }
